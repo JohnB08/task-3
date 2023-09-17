cc2693aae15513a387fd8e2ffc875c3fecfcd391
JohnB08 <johnb08.kodehode@gmail.com>
2023-09-17 12:21:58 +0200

    no gap top, some style change on menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const checkbox = document.getElementById("toggleSwitch");
 let textcontainer = document.getElementById("textwrapper");
 let label = document.getElementById("labelbtn");
+
+//This one is for the checkbox and label to function.
 checkbox.addEventListener("click", (event) => {
   if (checkbox.checked === true) {
     textcontainer.style.display = "flex";
@@ -15,6 +17,8 @@ const container = document.getElementById("ham-menu");
 let writing = document.getElementById("writingbox");
 let photo = document.getElementById("bookbox");
 let music = document.getElementById("musicbox");
+
+//This one makes the section show/hide different divs
 container.addEventListener("change", (event) => {
   if (container.value == 1) {
     writing.style.display = "block";
@@ -34,21 +38,31 @@ container.addEventListener("change", (event) => {
 });
 const stickyMenu = document.getElementById("stickymenu");
 const scrollY = window.scrollY;
+const menuCon = document.getElementById("menucon");
+const headingText = document.getElementById("headingtext");
+
+//This one moves the section menu to the right as you scroll, on portrait phones it covers the entire width.
 function moveMenu() {
   stickyMenu.style.display = "inline-block";
-  stickyMenu.style.transform = "translate(40vw,-5vh)";
+  stickyMenu.style.transform = "translate(40vw,-5.5vh)";
+  menuCon.style.borderTopRightRadius = "0";
+  menuCon.style.borderTopLeftRadius = "0";
   const curYPos = window.scrollY;
   if (curYPos === scrollY) {
     stickyMenu.style.transform = "translate(0vw, -5vh)";
+    menuCon.style.borderTopRightRadius = "0.5em";
+    menuCon.style.borderTopLeftRadius = "0.5em";
   }
 }
 function menublock() {
   stickyMenu.style.width = "100vw";
   stickyMenu.style.position = "fixed";
-  stickyMenu.style.transform = "translate(-50vw, -7vh)";
+  stickyMenu.style.transform = "translate(-50vw, -7.5vh)";
+  menuCon.style.borderRadius = "0";
+  headingText.style.color = "#403d39";
 }
 if (window.innerWidth <= 800) {
   window.addEventListener("scroll", menublock);
-} else {
+} else if (window.innerWidth > 800) {
   window.addEventListener("scroll", moveMenu);
 }
